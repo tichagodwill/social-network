e389a6799891cf71d929dea69814e9d796eea05b
afardan <lymfrdat@gmail.com>
2025-02-18 18:47:57 +0300

    fixed the up notification

--- server/pkg/db/migrations/sqlite/00015_add_invitation_id_to_notifications.up.sql
+++ server/pkg/db/migrations/sqlite/00015_add_invitation_id_to_notifications.up.sql
@@ -1,5 +1,5 @@
 -- Add invitation_id column to notifications table if it doesn't exist
-ALTER TABLE notifications ADD COLUMN invitation_id INTEGER REFERENCES group_invitations(id) ON DELETE CASCADE;
+-- ALTER TABLE notifications ADD COLUMN invitation_id INTEGER REFERENCES group_invitations(id) ON DELETE CASCADE;
 
 -- Create index for better performance
 CREATE INDEX IF NOT EXISTS idx_notifications_invitation_id ON notifications(invitation_id); 
